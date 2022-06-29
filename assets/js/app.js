cd3fe09b0cd203c8bcc873300abf6018d03086df
carlos_zenbook <scarlosenrique@gmail.com>
2022-06-29 05:24:43 -0500

    cargando files locales a git

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+alert("Hola ")
+
+console.log("Hola Mundo");
+
+let a = 10,
+    b = 20,
+    c = "Hola ",
+    d = "Mundo",
+    x = a + b;
+
+const saludo =c + d;
+
+console.log('%c Mis variables', 'color:blue; font-weight: bold');
+console.log({a});
+console.log({b});
+console.log({c});
+
+c = 'Hola Mundo';
+console.table({a,b,c,d, x});
